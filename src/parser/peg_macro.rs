1cd865bdfd119bd1dc6028c424f8503d3debb10e
gala377 <r.galczak@gmail.com>
2022-10-12 15:57:15 +0200

    Change macros to always refere to types with $crate

--- src/parser/peg_macro.rs
+++ src/parser/peg_macro.rs
@@ -3,18 +3,18 @@ macro_rules! field_op {
     ($tname:ident.$fname:ident) => {
         (
             |d: &$tname| d.$fname.clone(),
-            |d: &mut $tname, v: ParseRes<_>| d.$fname = Some(v),
+            |d: &mut $tname, v: $crate::parser::ParseRes<_>| d.$fname = Some(v),
         )
     };
 }
 #[macro_export]
 macro_rules! parse {
     ($sess:expr, $pos:expr, $fname:ident) => {
-        parse($sess, $pos, $crate::field_op!(Self.$fname), Self::$fname)
+        $crate::parser::parse($sess, $pos, $crate::field_op!(Self.$fname), Self::$fname)
     };
 
     ($sess:expr, $pos:expr, $tname:ident::$fname:ident) => {
-        parse(
+        $crate::parser::parse(
             $sess,
             $pos,
             $crate::field_op!($tname.$fname),
@@ -23,15 +23,38 @@ macro_rules! parse {
     };
 }
 
+/// Should be used only in the `peg` macro rules' bodies as it does not
+/// use parse! macro for memoization. Instead it assumes that the rule
+/// called handles memoization itself.
 #[macro_export]
 macro_rules! or {
     ($sess:expr, $pos:expr, $($rule:ident),+) => {
         $crate::or!(@unpack $sess, $pos, $($rule),+)
     };
     (@unpack $sess:expr, $pos:expr, $rule:ident, $($rest:ident),+) => {
-        match $crate::parse!($sess, $pos, $rule) {
-            res@ParseRes::Parsed { .. } => res,
-            ParseRes::NoParse => {
+        match Self::$rule($sess, $pos) {
+            res@$crate::parser::ParseRes::Parsed { .. } => res,
+            $crate::parser::ParseRes::NoParse => {
+                $crate::or!(@unpack $sess, $pos, $($rest),+)
+            }
+        }
+    };
+    (@unpack $sess:expr, $pos:expr, $rule:ident) => {
+        Self::$rule($sess, $pos)
+    };
+}
+
+/// Same as `or!` but calls rules with the `parse!` macro so the rules
+/// do not need to handle memoization themselves.
+#[macro_export]
+macro_rules! or_parse {
+    ($sess:expr, $pos:expr, $($rule:ident),+) => {
+        $crate::or!(@unpack $sess, $pos, $($rule),+)
+    };
+    (@unpack $sess:expr, $pos:expr, $rule:ident, $($rest:ident),+) => {
+        match crate::parse!($sess, $pos, $rule) {
+            res@$crate::parser::ParseRes::Parsed { .. } => res,
+            $crate::parser::ParseRes::NoParse => {
                 $crate::or!(@unpack $sess, $pos, $($rest),+)
             }
         }
@@ -41,19 +64,20 @@ macro_rules! or {
     };
 }
 
+#[macro_export]
 macro_rules! peg {
     (grammar $name:ident { $($rule:ident($sess:pat, $pos:pat) -> $t:ty $body:block)+ }) => {
 
         #[derive(Default, Clone, Debug)]
         pub struct $name {
             $(
-                $rule: Option<ParseRes<$t>>
+                $rule: Option<$crate::parser::ParseRes<$t>>
             ),+
         }
         impl $name {
             $(
-                fn $rule(sess: &mut Session<char, $name>, pos: usize) -> ParseRes<$t> {
-                    let f = |$sess: &mut Session<char, $name>, $pos: usize| -> ParseRes<$t> { $body };
+                fn $rule(sess: &mut $crate::parser::Session<char, $name>, pos: usize) -> $crate::parser::ParseRes<$t> {
+                    let f = |$sess: &mut $crate::parser::Session<char, $name>, $pos: usize| -> $crate::parser::ParseRes<$t> { $body };
                     parse(
                         sess,
                         pos,
